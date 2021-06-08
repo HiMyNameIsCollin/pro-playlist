01a538ad8ba04a28b8c5ffdf64b14f1f9d0c635b
Innuendo416 <collinstone2291@icloud.com>
2021-06-08 18:47:16 -0400

    Scroll bar, and fixed desktop overflow-x issue causing page to be under the scroll bar. NTS, max-width 100%

--- spotify/utils/handleViewArtist.js
+++ spotify/utils/handleViewArtist.js
@@ -5,6 +5,7 @@ export const handleViewArtist = ( e, artistArray, overlayFunc, activeItemFunc ) 
     if( artistArray.length === 1 ){
         activeItemFunc( artistArray[0] )
         overlayFunc( null )
+
     } else {
         const popupData = {
             title: 'Choose artist',
