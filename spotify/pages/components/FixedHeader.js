eab6cb1e54ce5f02dfdf51d61e9e620c90021026
Innuendo416 <collinstone2291@icloud.com>
2021-07-13 20:31:40 -0400

    ...

--- spotify/pages/components/FixedHeader.js
+++ spotify/pages/components/FixedHeader.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from 'react'
+import { useState, useEffect, useContext, useRef } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { DbHookContext } from './Dashboard'
 import { SearchHookContext } from './search/Search'
